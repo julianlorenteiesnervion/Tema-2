8dbd22e0d96e50e938b2fcca80be4f5900dc6e56
julian.lorente <julian.lorente@106-16.info2>
2024-10-07 12:46:38 +0200

    Ejercicio 6 NO terminado.

---
+++ src/boletinifelse/Ejer6.java
@@ -0,0 +1,32 @@
+package boletinifelse;
+
+import java.util.*;
+
+public class Ejer6 {
+
+	public static void main(String[] args) {
+		// Creación del Scanner
+		Scanner reader = new Scanner(System.in);
+		
+		// Declaración de variables
+		int a;
+		int b;
+		int c;
+		int x;
+		
+		// Mensajes iniciales para solicitar los valores de a, b y c
+		System.out.print("Introduce el valor de a: ");
+		a = reader.nextInt();
+		
+		System.out.print("Introduce el valor de b: ");
+		b = reader.nextInt();
+		
+		System.out.print("Introduce el valor de c: ");
+		c = reader.nextInt();
+		
+		// Operaciones
+		x = -b +
+
+	}
+
+}
