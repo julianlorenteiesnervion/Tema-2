bf485c7286dd9aedecd86574e28f3076c9c086ab
julian.lorente <julian.lorente@106-16.info2>
2024-10-08 14:34:48 +0200

    Ejercicio 7 NO terminado.

---
+++ src/boletinifelse/Ejer7.java
@@ -0,0 +1,19 @@
+package boletinifelse;
+
+import java.util.*;
+
+public class Ejer7 {
+
+	public static void main(String[] args) {
+		// Creación del Scanner
+		Scanner reader = new Scanner(System.in);
+		
+		// Declaración de la variable para el número introducido por el usuario
+		int num;
+		
+		// Solicitamos al usuario el número para que lo introduzca
+		System.out.print("Introduce un número comprendido entre 0 y 99999: ");
+
+	}
+
+}
