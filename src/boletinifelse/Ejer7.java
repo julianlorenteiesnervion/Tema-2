9cf309e012f5582d80ae1838fee8752ca7d18806
julia <julia@julian>
2024-10-08 17:19:56 +0200

    Ejercicio 7 terminado.

--- src/boletinifelse/Ejer7.java
+++ src/boletinifelse/Ejer7.java
@@ -13,6 +13,28 @@ public class Ejer7 {
 		
 		// Solicitamos al usuario el número para que lo introduzca
 		System.out.print("Introduce un número comprendido entre 0 y 99999: ");
+		num = reader.nextInt();
+		
+		/* Operaciones para descubrir el número de cifras que tiene. Aunque se puede hacer de formás más eficientes
+		*/
+		if (num < 0 || num > 99999) {
+            System.out.println("El número no está en el rango permitido.");
+        } else {
+            // Verificar cuántas cifras tiene el número
+            if (num < 10) {
+                System.out.println("El número tiene 1 cifra.");
+            } else if (num < 100) {
+                System.out.println("El número tiene 2 cifras.");
+            } else if (num < 1000) {
+                System.out.println("El número tiene 3 cifras.");
+            } else if (num < 10000) {
+                System.out.println("El número tiene 4 cifras.");
+            } else {
+                System.out.println("El número tiene 5 cifras.");
+            }
+        }
+		
+		reader.close();
 
 	}
 
