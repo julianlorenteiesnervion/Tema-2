c23df0056fac199461acd98d20e66cbc6570bfd6
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-10-18 14:00:32 +0200

    Ejercicio 2 terminado.

---
+++ src/boletindowhile/Ejer2.java
@@ -0,0 +1,17 @@
+package boletindowhile;
+
+public class Ejer2 {
+
+	public static void main(String[] args) {
+		// Declaro la variable num para mostrar los números pares
+		int num = 2;
+		
+		// Do-while para imprimir todos los números pares hasta el 200
+		do {
+			System.out.println(num);
+			num += 2;
+		} while (num <= 200);
+
+	}
+
+}
