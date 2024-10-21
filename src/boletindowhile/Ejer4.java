4c25df74212ddb6200bbc3a05c4d4685cfb39ef6
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-10-21 10:22:43 +0200

    Ejercicio 4 terminado.

--- src/boletindowhile/Ejer4.java
+++ src/boletindowhile/Ejer4.java
@@ -10,18 +10,21 @@ public class Ejer4 {
 		
 		/* Declaración de las variables num para el número
 		introducido por el usuario y num2 para usarlo como contador */
+		int num;
+		int num2 = 1;
 		
 		// Le solicitamos al usuario un número
 		System.out.print("Introduce un número: ");
-		int num = reader.nextInt();
+		num = reader.nextInt();
 		
-		int num2 = 1;
-		
+		/* Do-while para mostrar la tabla de multiplicar del número
+		 * introducido */
 		do {
 			System.out.println(num + " x " + num2 + " = " + (num * num2));
 			num2++;
 		} while (num2 <= 10);
-
+		
+		reader.close();
 	}
 
 }
