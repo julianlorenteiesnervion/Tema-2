60c5100df6e05f6e6429c0641922280441f44892
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-10-22 14:39:15 +0200

    Ejercicio 2 NO terminado.

---
+++ src/boletinfor/Ejer2.java
@@ -0,0 +1,15 @@
+package boletinfor;
+
+import java.util.*;
+
+public class Ejer2 {
+
+	public static void main(String[] args) {
+		// Creación del Scanner
+		Scanner reader = new Scanner(System.in);
+		
+		
+
+	}
+
+}
