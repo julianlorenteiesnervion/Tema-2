8e48b00873ac415afeb56a853afee7e9b8cee5f4
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-10-23 08:25:17 +0200

    Ejercicio 2 terminado.

--- src/boletinfor/Ejer2.java
+++ src/boletinfor/Ejer2.java
@@ -8,8 +8,21 @@ public class Ejer2 {
 		// Creación del Scanner
 		Scanner reader = new Scanner(System.in);
 		
+		// Declaración de las variables
+		// Variable para guardar el número que nos introduce el usuario
+		int num;
 		
-
+		// Preguntamos al usuario por el número
+		System.out.print("Introduce un número: ");
+		num = reader.nextInt();
+		
+		// For
+		/* Iniciamos en 0, seguiremos repitiendo el bucle mientras i sea menor o igual que
+		 * el número introducido y sumaremos 3 por cada vuelta a i */
+		for (int i = 0; i <= num; i+=3) {
+			System.out.println(i);
+		}
+		reader.close();
 	}
 
 }
