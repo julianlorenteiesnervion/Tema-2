686eec08fc433dba9812dc21b0a955a7871b7197
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-10-23 09:14:24 +0200

    Ejercicio 4 terminado.

--- src/boletinfor/Ejer4.java
+++ src/boletinfor/Ejer4.java
@@ -6,14 +6,17 @@ public class Ejer4 {
 		// Declaramos la variable impar para actualizar y mostrar los números impares
 		int impar = 1;
 		
+		// Variable para guardar la suma de los impares
+		int sumaImpares = 1;
+		
 		/* For en el que, empezamos con 1, continuamos mientras i sea menor
-		 * o igual que 10 para así dar 10 vueltas y sumamos un 1 cada vuelta */
-		for (int i = 1; i <= 10; i++) {
-			System.out.println(i + "º = " + impar); // Imprimimos el número impar
-			
-			impar += 2; // Le sumamos 2 al número impar
+		 * que 10 para así dar 10 vueltas y sumamos un 1 cada vuelta */
+		for (int i = 1; i < 10; i++) {
+			sumaImpares += impar += 2; // Le sumamos el número impar a la suma total de impares
 		}
-
+		
+		System.out.println(sumaImpares);
+		
 	}
 
 }
